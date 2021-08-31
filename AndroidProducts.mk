9b2cef3427fd8da4ee94a6a05df469d90177c1a2
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-08-31 16:01:10 +0800

    RevengeOS: ulova: Initial Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -11,3 +11,11 @@ COMMON_LUNCH_CHOICES := \
     lineage_ulova-user \
     lineage_ulova-userdebug \
     lineage_ulova-eng
+
+PRODUCT_MAKEFILES += \
+    $(LOCAL_DIR)/revengeos_ulova.mk
+
+COMMON_LUNCH_CHOICES += \
+    revengeos_ulova-user \
+    revengeos_ulova-userdebug \
+    revengeos_ulova-eng
---
+++ revengeos_ulova.mk
@@ -0,0 +1,33 @@
+#
+# Copyright (C) 2021 The RevengeOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
+
+# Inherit some common RevengeOS stuff.
+$(call inherit-product, vendor/revengeos/config/common.mk)
+
+# Inherit from ulova device
+$(call inherit-product, device/xiaomi/ulova/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := ulova
+PRODUCT_NAME := revengeos_ulova
+BOARD_VENDOR := Xiaomi
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi 4A / 5A / Note 5A / Y1
+PRODUCT_MANUFACTURER := Xiaomi
+TARGET_VENDOR := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="ugg-user 7.1.2 N2G47H V11.0.2.0.NDKMIXM release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := "xiaomi/ugg/ugg:7.1.2/N2G47H/V11.0.2.0.NDKMIXM:user/release-keys"
