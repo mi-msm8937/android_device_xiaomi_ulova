jaguarezz/android_device_xiaomi_ulysse

ulysse: biometrics: Introduce FPC+Goodix loading mechanism

biometrics/BiometricsFingerprint.h
@@ -63,6 +63,8 @@ public:
 
 private:
     static fingerprint_device_t* openHal();
+    static fingerprint_device_t* openTheHal(const char *class_name);
+    static void setFpSensorProp(std::string hwmdl_name);
     static void notify(const fingerprint_msg_t *msg); /* Static callback for legacy HAL implementation */
     static Return<RequestStatus> ErrorFilter(int32_t error);
     static FingerprintError VendorErrorFilter(int32_t error, int32_t* vendorCode);

mi-msm8937/android_device_xiaomi_ulova

biometrics/BiometricsFingerprint.h
@@ -63,6 +63,8 @@ public:
 
 private:
     static fingerprint_device_t* openHal();
+    static fingerprint_device_t* openTheHal(const char *hwmdl_name);
+    static void setFpSensorProp(std::string hwmdl_name);
     static void notify(const fingerprint_msg_t *msg); /* Static callback for legacy HAL implementation */
     static Return<RequestStatus> ErrorFilter(int32_t error);
     static FingerprintError VendorErrorFilter(int32_t error, int32_t* vendorCode);